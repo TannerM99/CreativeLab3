7c40e8c64cd1accb6dc26047c2468b5668b5f86f
Tanner Manwaring <tb.manwaring@yahoo.com>
2022-10-29 18:12:39 +0000

    refresh

--- cl3/src/pages/Layout.js
+++ cl3/src/pages/Layout.js
@@ -3,18 +3,20 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
+      <nav class="navbar navbar-expand-sm bg-light">
+        <div class="container-fluid">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <Link class="nav-link" to="/">Home</Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/blogs">Blogs</Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
 
       <Outlet />
